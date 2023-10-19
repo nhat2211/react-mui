052e4155140ac073d59146049ed38b4490994295
nhat2211 <tranquangnhat2211@gmail.com>
2023-10-19 14:47:26 +0700

    add json-server

---
+++ src/components/AddUser.js
@@ -0,0 +1,39 @@
+import { Button, TextField } from "@mui/material";
+import Grid from "@mui/material/Grid";
+
+
+export default function AddUser() {
+
+    return(
+
+        <div>
+
+<Grid container spacing={2}>
+  <Grid item xs={12}>
+   
+    <form>
+                <TextField id="name" label="name">
+
+                </TextField>
+
+                <TextField id="username" label="username">
+
+                 </TextField>
+
+                 <Button variant="contained">Add User</Button>
+
+            </form>
+
+    
+  </Grid>
+ 
+</Grid>
+
+
+
+        </div>
+        
+            
+       
+    )
+}
