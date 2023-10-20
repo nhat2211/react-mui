aab9ae7fbf0f1505c358589ccebc965a19ae7b72
nhat2211 <tranquangnhat2211@gmail.com>
2023-10-20 22:43:46 +0700

    complete home page with staff list

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,17 @@ import { Paper, TableContainer } from "@mui/material";
 import React from "react"
 import { useEffect, useState } from "react"
 import {Table, TableHead,TableRow,TableCell, TableBody} from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+import Icon from "@mui/material/Icon";
+import { green } from "@mui/material/colors";
+import Stack from "@mui/material/Stack";
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(id, createdAt, name, avatar, age, address) {
+    return { id, createdAt, name, avatar, age, address };
   }
 
   const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    
   ];
 
 export default function Home() {
@@ -43,26 +43,45 @@ export default function Home() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="left">Created At</TableCell>
+            <TableCell align="left">Avatar</TableCell>
+            <TableCell align="left">Age</TableCell>
+            <TableCell align="left">Address</TableCell>
+            <TableCell align="left">Action</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {APIData.map((staff) => (
             <TableRow
-              key={row.name}
+              key={staff.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
+                 <TableCell component="th" scope="row">
+                {staff.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell component="th" scope="row">
+                {staff.name}
+              </TableCell>
+              <TableCell align="left">{staff.createdAt}</TableCell>
+              <TableCell align="right">
+            
+                <Avatar align="left" alt="Remy Sharp" src={staff.avatar} />
+                
+                </TableCell>
+              <TableCell align="left">{staff.age}</TableCell>
+              <TableCell align="left">{staff.address}</TableCell>
+              <TableCell align="left">
+              <Stack direction="row" spacing={3}>
+              <Icon sx={{ color: green[500] }}>add_circle</Icon>
+              <Icon sx={{ color: green[500] }}>update_circle</Icon>
+              <Icon sx={{ color: green[500] }}>delete_circle</Icon>
+
+
+              </Stack>
+                
+                </TableCell>
             </TableRow>
           ))}
         </TableBody>
